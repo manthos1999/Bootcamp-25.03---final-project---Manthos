54c948d4ae404ba23a692af392d773a126307c0a
Manthos <mgkalimanis@gmail.com>
2025-06-16 00:37:55 +0300

    Implement login flow, chat page, and message persistence
    - Created login API and login page
    - Built chat page using Next.js and React
    - Updated createMessageAndGetCompletion to persist messages in the database

---
+++ src/main/java/bootcamp_2025_03_manthos/model/TokenDTO.java
@@ -0,0 +1,12 @@
+package bootcamp_2025_03_manthos.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDTO {
+    private String token;
+}
